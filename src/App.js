624b1e085c1347b1c2054babe08a085b38c23df2
Dylyn-Daughtry <98605975+Dylyn-Daughtry@users.noreply.github.com>
2022-03-15 17:19:08 -0400

    Search bar Created and functional

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import NavBar from './components/NavBar/NavBar';
 import './App.css'
 import axios from 'axios';
 import Modal from './components/modal/modal';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
 
   const[hideShow,setHideShow]=useState(false)
   const [songs, setSongs] = useState([]);
+  const [displaySongs, setDisplaySongs] = useState([])
   
   useEffect(() => {
     if (hideShow === false) getAllSongs();
@@ -28,16 +30,43 @@ function App() {
   async function getAllSongs(prop){
     let response = await axios.get('http://127.0.0.1:8000/api/music/');
     setSongs(response.data);
-    console.log(response.data)
+    setDisplaySongs(response.data)
+    
   }
+  async function deleteSong(prop){
+    let response = await axios.delete('http://127.0.0.1:8000/api/music/2/', prop)
+  }
+
+  const filterSongs = (searchterm) => {
+    
+    let matchingSongs = songs.filter((song) => {
+      if(song.title.toLowerCase().includes(searchterm.toLowerCase()) ||
+
+      song.artist.toLowerCase().includes(searchterm.toLowerCase()) || 
+
+      song.album.toLowerCase().includes(searchterm.toLowerCase) || 
+
+      song.release_date.toLowerCase().includes(searchterm.toLowerCase) || 
+
+      song.genre.toLowerCase().includes(searchterm.toLowerCase)) 
+        
+      return true
+    
+    else return false
+  })
+
+  setDisplaySongs(matchingSongs)
+};
 
   return (
     <div>
         <NavBar createSong={createSong} createOnClick={handleHideShow}/>
-        <MainDisplay songs={songs}/>
+        <SearchBar filterSongs={filterSongs}/>
+        <MainDisplay songs={displaySongs}/>
         <Modal onClick={handleHideShow}hideShow={hideShow}><CreateSong createSong={createSong} /></Modal>
     </div>
   );
   }
+// }
 
 export default App;
